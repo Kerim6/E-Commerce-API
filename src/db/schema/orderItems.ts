75ef2e0dd9f85ddebb29577fb594bbe7d4732941
Abdulrahman Krayem <kylwa2077@gmail.com>
2026-07-13 16:12:07 +0300

    Add order items and orders schema definitions with UUIDs, references, and timestamps

---
+++ src/db/schema/orderItems.ts
@@ -0,0 +1,24 @@
+import {
+  pgTable,
+  uuid,
+  varchar,
+  numeric,
+  integer,
+  timestamp,
+} from "drizzle-orm/pg-core";
+import { orders } from "./orders.ts";
+import { products } from "./products.ts";
+
+export const orderItems = pgTable("order_items", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  orderId: uuid("order_id")
+    .references(() => orders.id)
+    .notNull(),
+  productId: uuid("product_id")
+    .references(() => products.id)
+    .notNull(),
+  unitPrice: numeric("unit_price").notNull(),
+  quantity: integer("quantity").notNull(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("created_at").notNull().defaultNow(),
+});
