bc442d1c22d464397919e9bb6c542c0f9dbd5414
Abdulrahman Krayem <kylwa2077@gmail.com>
2026-07-13 15:55:19 +0300

    Add cart items schema definition with UUID, cartId, productId, quantity, and constraints

---
+++ src/db/schema/cartItems.ts
@@ -0,0 +1,22 @@
+import { pgTable, uuid, integer, check, unique } from "drizzle-orm/pg-core";
+import { carts } from "./carts.ts";
+import { products } from "./products.ts";
+import { sql } from "drizzle-orm";
+
+export const cartItems = pgTable(
+  "cart_items",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    cartId: uuid("cart_id")
+      .references(() => carts.id)
+      .notNull(),
+    productId: uuid("product_id")
+      .references(() => products.id)
+      .notNull(),
+    quantity: integer("quantity").notNull(),
+  },
+  (table) => [
+    check("quantity_non_negative", sql`${table.quantity} >0`),
+    unique("cart_product_unique").on(table.cartId, table.productId),
+  ],
+);
