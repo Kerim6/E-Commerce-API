fa6df123cc4730791a130d3bc5e440439364d0e9
Abdulrahman Krayem <kylwa2077@gmail.com>
2026-08-04 18:00:40 +0300

    Add tests for category retrieval and validation; enhance unauthenticated request handling

--- src/tests/categories.test.ts
+++ src/tests/categories.test.ts
@@ -2,6 +2,7 @@ import { createTestUser } from './helpers/authHelpers.ts'
 import { createTestCategory } from './factories/categoryFactory.ts'
 import request from 'supertest'
 import app from '../server.ts'
+import { ca } from 'zod/locales'
 
 describe('POST /api/v1/categories', () => {
   it('should allow an admin to create a category', async () => {
@@ -71,5 +72,65 @@ describe('POST /api/v1/categories', () => {
       .send({ name: 'fasion', slug: '' })
 
     expect(response.status).toBe(400)
+  })
+})
+
+describe('get /api/v1/categories', () => {
+  it('should get all categories', async () => {
+    const user = await createTestUser()
+
+    const admin = await createTestUser({ role: 'admin' })
+    await createTestCategory(admin.token)
+    await createTestCategory(admin.token)
+    await createTestCategory(admin.token)
+
+    const response = await request(app)
+      .get('/api/v1/categories')
+      .set('Authorization', `Bearer ${user.token}`)
+
+    expect(response.status).toBe(200)
+    expect(Array.isArray(response.body)).toBe(true)
+    expect(response.body.length).toBeGreaterThanOrEqual(2)
+  })
+
+  it('should reject unauthenticated request', async () => {
+    const response = await request(app).get('/api/v1/categories')
+
+    expect(response.status).toBe(401)
+  })
+
+  it('should get one category', async () => {
+    const user = await createTestUser()
+    const admin = await createTestUser({ role: 'admin' })
+    const category = await createTestCategory(admin.token)
+
+    const response = await request(app)
+      .get(`/api/v1/categories/${category.category.id}`)
+      .set('Authorization', `Bearer ${user.token}`)
+
+    expect(response.status).toBe(200)
+    expect(response.body.id).toBe(category.category.id)
+    expect(response.body.name).toBe(category.input.name)
+    expect(response.body.slug).toBe(category.input.slug)
+  })
+
+  it('should reject an invalid id', async () => {
+    const user = await createTestUser()
+
+    const response = await request(app)
+      .get('/api/v1/categories/invalid-id')
+      .set('Authorization', `Bearer ${user.token}`)
+
+    expect(response.status).toBe(400)
+  })
+
+  it('should return 404 when category does not exist', async () => {
+    const user = await createTestUser()
+
+    const response = await request(app)
+      .get('/api/v1/categories/123e4567-e89b-12d3-a456-426614174000')
+      .set('Authorization', `Bearer ${user.token}`)
+
+    expect(response.status).toBe(404)
   })
 })
